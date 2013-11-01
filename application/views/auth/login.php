5b343cd876659143197379bbd1a76490b88d81a0
Steven Enamakel <stevent95@gmail.com>
2013-11-01 14:11:21 -0400

    Login page draft #2
    Added some more stuff for the login page, like a blurred background, a
    sticky footer and added mobile compatibility.

--- application/views/auth/login.php
+++ application/views/auth/login.php
@@ -4,23 +4,26 @@
     $this->load->view('common/header', $data);
 ?>
         <section id="main-body">
+            <!--The login window div-->
             <section id="login-window">
+                <!--Foundation's javascript section-->
                 <div data-section="" class="section-container auto font-title" style="min-height: 51px;" data-section-resized="true">
                     <section class="dropdown active">
-                        <div data-section-title="" class="title"><a href="#panel1">Login</a></div>
+                        <div data-section-title="" class="title"><a href="#panel1"><span class="font-awesome mobile-tab-icon hide-for-medium-up"></span>Login</a></div>
                         <div data-section-content="" class="content">
+                            <!--Login form-->
                             <form action="login" method="post" enctype="multipart/form-data">
                                 <p class="text-center" style="color:#747474;padding: 30px 0">>LOGO HERE<</p>
                                 <div class="row">
                                     <div class="large-4 columns form-text">Username</div>
                                     <input type="text" class="form-input large-8 columns" placeholder="who are you?">
                                 </div>
-                                </br>
+                                <br/>
                                 <div class="row">
                                     <div class="large-4 columns form-text">Password</div>
                                     <input type="password" class="form-input large-8 columns" placeholder="to make sure it is really you..">
                                 </div>
-                                </br>
+                                <br/><br/>
                                 <div class="row">
                                     <input type="submit" class="form-submit large-3 large-centered columns" value="Login">
                                 </div>
@@ -30,36 +33,37 @@
                         </div>
                     </section>
                     <section class="dropdown">
-                        <div data-section-title="" class="title" style="left: 89px;"><a href="#panel2">Register</a></div>
+                        <div data-section-title="" class="title" style="left: 89px;"><a href="#panel2"><span class="font-awesome mobile-tab-icon hide-for-medium-up"></span>Sign Up</a></div>
                         <div data-section-content="" class="content">
+                            <!--Registration form-->
                             <form action="register" method="post" enctype="multipart/form-data">
                                 <p class="text-center" style="color:#747474;padding: 30px 0">>LOGO HERE<</p>
                                 <div class="row">
                                     <div class="large-4 columns form-text">New Username</div>
                                     <input type="text" class="form-input large-8 columns" placeholder="a unique name to identify you">
                                 </div>
-                                </br>
+                                <br/>
                                 <div class="row">
                                     <div class="large-4 columns form-text">Email</div>
                                     <input type="email" class="form-input large-8 columns" placeholder="whichever email you prefedasd">
                                 </div>
-                                </br>
+                                <br/>
                                 <div class="row">
                                     <div class="large-4 columns form-text">New Password</div>
                                     <input type="password" class="form-input large-8 columns" placeholder="something you won't forget">
                                 </div>
-                                </br>
+                                <br/>
                                 <div class="row">
                                     <div class="large-4 columns form-text">Re-enter Password</div>
                                     <input type="password" class="form-input large-8 columns" placeholder="to make sure you typed it correctly">
                                 </div>
-                                </br>
+                                <br/><br/>
                                 <div class="row">
-                                    <input type="submit" class="form-submit large-3 large-centered columns" value="Register">
+                                    <input type="submit" class="form-submit large-3 large-centered columns" value="Sign Up">
                                 </div>
                             </form>
                             <div class="seperator"></div>
-                            <p>Login using a social network</p>
+                            <p>Sign up using a social network</p>
                         </div>
                     </section>
                 </div>
--- application/views/i/footer.php
+++
@@ -1,8 +0,0 @@
-            </div>
-            <footer id="footer">
-                <p id="version">v<?php echo config_item('version').' | Generated in '.$this->benchmark->elapsed_time().' seconds'; ?></p>
-                <p id="legal">Apple For Orange Inc.</p>
-            </footer>
-        </div>
-    </body>
-</html>
