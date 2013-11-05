219d76d77e6aa5e8c47d7737781b27ef132d1e6d
Steven Enamakel <stevent95@gmail.com>
2013-11-05 17:56:17 -0500

    Added mobile support for navigational header

--- application/views/home.php
+++ application/views/home.php
@@ -4,9 +4,6 @@
     $data['showNavBar'] = True;
     $this->load->view('common/header', $data);
 ?>
-        <nav>
-          
-        </nav>
         <section id="main-body">
         </section>
 <?php $this->load->view('common/footer'); ?>
