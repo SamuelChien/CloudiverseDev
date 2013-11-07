f64910deac3e818e36ac57b076633ed7103a47cd
Samoo <samuelchien821@gmail.com>
2013-11-06 22:11:28 -0500

    Delete all the files with merging issues

--- application/views/home.php
+++
@@ -1,9 +0,0 @@
-<?php
-    $data['bodyID'] = "home-page";
-    $data['pageTitle'] = "Welcome";
-    $data['showNavBar'] = True;
-    $this->load->view('common/header', $data);
-?>
-        <section id="main-body">
-        </section>
-<?php $this->load->view('common/footer'); ?>
--- www/asset/sass/startSASS.bat
+++
@@ -1,2 +0,0 @@
-sass --watch foundation.scss:..\css\foundation.css style.scss:..\css\style.css
-pause
