7ffc0399e2edca1f14889f86d786834b37cd3d2c
Steven Enamakel <stevent95@gmail.com>
2013-12-02 19:42:32 -0500

    Foundation CSS + JS patch. Progress bar.
    Adjustments made to the SASS/CSS and JS files to work with the new
    foundation framework. the HTML patch will proceed after this commit.
    
    Added CSS for a simple progress bar.

--- www/asset/js/cloudiverse.js
+++ www/asset/js/cloudiverse.js
@@ -7,5 +7,16 @@ $(document).ready(function() {
 		alert($("#loginAccount").val());
 		alert($("#loginPassword").val());
     });
-
 });
+
+/* A function used to set the value of the progress bar.
+ *
+ * value:           a value between 0 and 1.
+ * jQueryObj:       the jQueryObj variable identifying that progress bar.
+ * afterFunction:   the function that gets called after the animation is complete.
+ */
+function setProgressBar(value, jQueryObj, afterFunction) {
+    jQueryObj.stop();
+    width100 = jQueryObj.width();
+    jQueryObj.find(".status").animate({width: width100 * value}, 2000, afterFunction);
+}
--- www/asset/js/fileCanvas.js
+++ www/asset/js/fileCanvas.js
@@ -1,36 +1,89 @@
-$(function() 
-  {
-    $( "#sortable" ).sortable();
-    $( "#sortable" ).disableSelection();
+$(document).ready(function() {
 
-    $("#addFolder").click(function() {
-        $("#sortable").append("<li class='ui-state-default'>" + 
-                                "<ul id='inline'>" + 
-                                    "<li id='pictures'><img class='image' src='/asset/img/folder.png'></li>" + 
-                                    "<li id='name'>Folder</li>" + 
-                                    "<li id='time'>Time</li>" + 
-                                "</ul>" + 
-                            "</li>");
+  var filelist = $("#file-list ul");
+
+  //filelist.sortable();
+  //filelist.disableSelection();
+
+  function sortFilelistHTML(key, order){
+    var fileArray = [];
+    var folderArray = [];
+    var sorted_html_code = "";
+    function geneareHTMLlist(objArray){
+      ret = ""
+      for (var i=0; i<objArray.length; i++)
+        ret += '<li class="'+ objArray[i].type +'">' + objArray[i].obj.html() + '</li>';
+      return ret;
+    }
+
+    // Start iterating through each file and put it's details into an array.
+    filelist.find("li").each(function(){
+      var that = $(this)
+
+      if (that.attr("class") == "type-folder") {
+        folderArray.push({
+          name: that.find(".name").html(),
+          type: that.attr("class"),
+          dateModified: that.find(".date-modified").html(),
+          obj: that
+        });
+      } else{
+        fileArray.push({
+          name: that.find(".name").html(),
+          type: that.attr("class"),
+          dateModified: that.find(".date-modified").html(),
+          obj: that
+        });
+      }
     });
 
-    $("#addPDF").click(function() {
-        $("#sortable").append("<li class='ui-state-default'>" + 
-                                "<ul id='inline'>" + 
-                                    "<li id='pictures'><img class='image' src='/asset/img/pdf.jpg'></li>" + 
-                                    "<li id='name'>PDF</li>" + 
-                                    "<li id='time'>Time</li>" + 
-                                "</ul>" + 
-                            "</li>");
-    });
-    
-    $("#addWord").click(function() {
-        $("#sortable").append("<li class='ui-state-default'>" + 
-                                "<ul id='inline'>" + 
-                                    "<li id='pictures'><img class='image' src='/asset/img/word.png'></li>" + 
-                                    "<li id='name'>Word</li>" + 
-                                    "<li id='time'>Time</li>" + 
-                                "</ul>" + 
-                            "</li>");
-    });
+    function compare_name(a,b) {
+      if(a.name < b.name)
+         return -1;
+      if(a.name > b.name)
+         return 1;
+      return 0;
+    }
+
+    fileArray.sort(compare_name);
+    folderArray.sort(compare_name);
+
+    if (order == -1) {
+      fileArray.reverse();
+      folderArray.reverse();
+    }
+    // Generate the HTML code for the new sorted list.
+    sorted_html_code = geneareHTMLlist(folderArray) + geneareHTMLlist(fileArray);
+
+    // replace the old HTML with the sorted HTML code.
+    filelist.html(sorted_html_code);
   }
-);
+
+  sortFilelistHTML('name');
+
+  // If the "Create Folder" button was pressed, add a new folder
+  $("#newF-button").click(function() {
+      filelist.append('<li class="type-folder">\
+          <div class="name">New Folder</div>\
+          <div class="icon share-link">&#xf14c;</div>\
+          <div class="icon delete-link">&#xf00d;</div>\
+          <div class="icon info-link">&#xf129;</div>\
+          <div class="filetype">Folder</div>\
+          <div class="date-modified">Just Now</div>\
+        </li>');
+      sortFilelistHTML('name');
+  });
+
+  // If the "Upload" button was pressed, add a new file
+  $("#upload-button").click(function() {
+      filelist.append('<li class="type-file">\
+          <div class="name">New File</div>\
+          <div class="icon share-link">&#xf14c;</div>\
+          <div class="icon delete-link">&#xf00d;</div>\
+          <div class="icon info-link">&#xf129;</div>\
+          <div class="filetype">File</div>\
+          <div class="date-modified">Just Now</div>\
+        </li>');
+      sortFilelistHTML('name');
+  });
+});
